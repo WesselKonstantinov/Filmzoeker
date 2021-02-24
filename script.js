e1ab72b2ad7fd6e9d16f7873c4fffcebfb548ca2
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-02-24 14:40:32 +0100

    Further adjustments made to script and index.html

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-// Filteren op laatste films (2014 of nieuwer) => filteren op year en
-// checken dat het movies.Year >= 2014
-
-// Filteren op naam in de titel => een functie die de naam als argument neemt
-// In de functie zelf wordt met behulp van includes op movies.Title gecheckt
-// of de bewuste naam in de titel zit. Hierop gaan we filteren
-
-/* 
-    A function that creates a list element out of each movie and appends it 
-    to the list of movies
-*/
+// This function creates a list element out of each movie and appends it to the list of movies
 const addMoviesToDOM = movies => {
-    const moviesList = document.querySelector('.site-main__movies-list');
+    const moviesList = document.querySelector('#movies-list');
     movies.forEach(movie => {
-        const moviesListItem = document.createElement('li');
-        moviesListItem.innerHTML = `<img src="${movie.Poster}">`;
-        moviesList.appendChild(moviesListItem);
-    })
+        const newMoviesListItem = document.createElement('li');
+        newMoviesListItem.innerHTML = `<img src="${movie.Poster}">`;
+        moviesList.appendChild(newMoviesListItem);
+    });
 };
 
-addMoviesToDOM(movies);
+// This function filters movies by checking if a word is present in the movie title
+const filterMovies = wordInMovieTitle => {
+    const filteredMovies = movies.filter(movie => movie.Title.includes(wordInMovieTitle));
+    addMoviesToDOM(filteredMovies);
+};
+
+// This function filters movies from 2014 and later
+const filterLatestMovies = () => {
+    const filteredMovies = movies.filter(movie => Number(movie.Year) >= 2014);
+    addMoviesToDOM(filteredMovies);
+};
+
+// Attach an event handler to all radio buttons
+const radioButtons = document.querySelectorAll('.site-nav__radio');
+radioButtons.forEach(radioButton => {
+    radioButton.addEventListener('change', e => {
+        // Change display of movies based on the category that has been chosen
+        const movieCategory = e.target.value;
+        switch (movieCategory) {
+            case 'latest-movies':
+                filterLatestMovies();
+                break;
+            case 'avenger-movies':
+                filterMovies('Avengers');
+                break;
+            case 'x-men-movies':
+                filterMovies('X-Men');
+                break;
+            case 'princess-movies':
+                filterMovies('Princess');
+                break;
+            case 'batman-movies':
+                filterMovies('Batman');
+                break;
+        };
+    });
+});
